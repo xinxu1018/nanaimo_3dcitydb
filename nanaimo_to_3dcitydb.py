b66b831572e042f2b50ae24a2f5281aacca64a89
Isaac Boates <iboates@gmail.com>
2018-04-25 18:56:11 +0200

    cleaned up repo for paper publishing

--- nanaimo_to_3dcitydb.py
+++ nanaimo_to_3dcitydb.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = psycopg2.connect("host=localhost dbname=unade user=postgres password=postgres")
     cur = conn.cursor()
 
-    citygml = ET.parse('/media/isaac/Data/_GMCM/Thesis/data/citygml/nanaimo_water_network.gml')
+    citygml = ET.parse('data/nanaimo_water_network.gml')
 
     import_Network(citygml, cur)
 

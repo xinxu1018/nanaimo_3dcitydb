2aefddde839909d6986e7d2f2e3f3c100ed3bfe5
Isaac Boates <iboates@gmail.com>
2018-10-02 17:47:08 +0200

    Update network_volume.sql

--- sql/network_volume.sql
+++ sql/network_volume.sql
@@ -1,32 +1,8 @@
-﻿DROP TABLE IF EXISTS public.nanaimo_routing;
-
-CREATE TABLE public.nanaimo_routing AS (
-
-	SELECT
-		oc.classname,
-		l.id,
-		l.gmlid,
-		l.start_node_id,
-		l.end_node_id,
-		ST_Length(l.line_geom) AS cost,
-		ST_Length(l.line_geom) AS reverse_cost,
-		l.line_geom AS geom
-	FROM
-		citydb.objectclass AS oc,
-		citydb.utn9_link AS l
-	WHERE
-		oc.id = l.objectclass_id
-
-);
-
-DROP TABLE IF EXISTS public.nanaimo_network_volume;
-
-CREATE TABLE public.nanaimo_network_volume AS (
-	SELECT DISTINCT ON (l.id)
-		l.id AS id,
-		l.feat_graph_id,
-		rp.ext_diameter AS diameter,
-		l.line_geom AS geom
+WITH total_volume AS (
+	SELECT DISTINCT ON (rp.id)
+		--rp.id AS id,
+		--l.feat_graph_id,
+		(rp.ext_diameter/200) * pi() * ST_Length(rp.geom) AS volume
 	FROM
 		pgr_drivingDistance(
 
@@ -91,4 +67,8 @@ CREATE TABLE public.nanaimo_network_volume AS (
 		fg.ntw_feature_id = rp.id
 		AND
 		rp.ext_diameter != 0
-);
+	GROUP BY
+		rp.id, rp.ext_diameter, rp.geom
+)
+
+SELECT SUM(volume) FROM total_volume;
